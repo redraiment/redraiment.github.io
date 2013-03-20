8ca9809eaf1af209edef0f727279f6f719361ee4
redraiment <redraiment@gmail.com>
2013-03-20 21:17:26 +0800

    Mobile version

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,5 +9,5 @@ bin/generate-nav.sh > data/navigator.json
 # local | remote
 target=${1:-local}
 
-rm -rf ${target}/{app,data,resources,index.html}
-cp -r app data resources index.html "${target}/"
+rm -rf ${target}/{app,data,resources,mobile,index.html}
+cp -r app data resources mobile index.html "${target}/"
