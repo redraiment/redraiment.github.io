ad0a201e35d4a308ac9ffd8894bedecbbf33d2ac
redraiment <redraiment@gmail.com>
2013-03-21 08:57:02 +0800

    Add Google Analytics to Mobile Version

--- resources/js/mobile.js
+++ resources/js/mobile.js
@@ -9,3 +9,9 @@ ajax.get(url, function(text) {
     text = text.replace(/<script.+<\/script>/g, '');
     document.body.innerHTML = text;
 });
+
+// For Google Analytics
+var _gaq = [
+    ['_setAccount', 'UA-39440649-1'],
+    ['_trackPageview']
+];
