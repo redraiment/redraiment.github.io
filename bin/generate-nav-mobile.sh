da1c5f0a9c8b8bdfd9f007a13e2127e302e2bc20
redraiment <redraiment@gmail.com>
2013-03-26 14:59:14 +0800

    Google Docs发表博文

--- bin/generate-nav-mobile.sh
+++ bin/generate-nav-mobile.sh
@@ -17,12 +17,12 @@ find data -type d | while read d; do
                 -e 's/algorithm/算法/g' \
                 -e 's/hdu/杭电100题/g' \
                 -e 's/kill-if/消灭成堆的分支语句/g' \
-                -e 's/code/物尽其（奇）用/g' \
+                -e 's/code/代码分享/g' \
                 -e 's/FriendSell/好友买卖外挂/g' \
                 -e 's/basic/BASIC解释器/g' \
                 -e 's/script/脚本小子/g' \
                 -e 's/thinking/思考与感悟/g' \
-                -e 's/tool-tips/善用佳软/g' \
+                -e 's/tool-tips/物尽其（奇）用/g' \
                 -e 's/rack-my-brain/拍拍脑袋/g' \
                 -e 's/reading-room/阅读室/g' \
                 -e 's/learning/学习方法/g' \
