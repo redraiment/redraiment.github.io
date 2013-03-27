b97766a911a5257a2092c981ef5ec7a9de8522bb
redraiment <redraiment@gmail.com>
2013-03-27 23:08:38 +0800

    C语言上级实验报告

--- bin/generate-nav.js
+++ bin/generate-nav.js
@@ -116,6 +116,7 @@ var getFileTitle = function(file) {
 var i18n_zh_CN = {
     'software': '软件人生',
     'cpp': 'C语言',
+    'experiment': '上机实验报告',
     'programming': '程序设计方法',
     'algorithm': '算法',
     'hdu': '杭电100题',
